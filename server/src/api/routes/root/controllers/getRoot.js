b99188ec37505a47b5735d259b557ca44b3e80ac
Alexey Soloviev <alexey.soloviev.w@gmail.com>
2020-06-15 21:36:28 +0300

    Fix building issues

--- server/src/api/routes/root/controllers/getRoot.js
+++ server/src/api/routes/root/controllers/getRoot.js
@@ -1,9 +1,7 @@
 import path from "path";
 
-import sendResponse from "@utils/http/sendResponse";
-
 export default async (request, response) => {
     const root = process.cwd();
     const indexHtml = path.join(root, "..", "client", "build", "index.html");
-    sendResponse(response, indexHtml);
+    response.sendFile(indexHtml);
 };
